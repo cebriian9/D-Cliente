b2013f128d8f793f3c00883891471f1e15947d14
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-11-17 10:52:17 +0100

    matriculasao-calse

--- JavaScript/T1/tema3/T3P1_Ejercicio-Matriculas/index.js
+++ JavaScript/T1/tema3/T3P1_Ejercicio-Matriculas/index.js
@@ -18,6 +18,9 @@ function calcularA() {
     let PORdp = 0 //doble pareja 
     //-----------------porcentajes %
 
+    let encontrado = false
+
+
 
     for (let i = 0; i < 10000; i++) {
         //mostrar array orignal
@@ -33,43 +36,49 @@ function calcularA() {
 
 
         //comporbar si es un poker
-        if (nums[0] == nums[1] && nums[1] == nums[2]
+        if (encontrado == false && nums[0] == nums[1] && nums[1] == nums[2]
             && nums[2] == nums[3] && nums[3] == nums[0]) {
 
             poker++
+            encontrado = true
         }
 
         //escalera completa
-        if ((numsHelp[0] + 1) == numsHelp[1] && (numsHelp[1] + 1) == numsHelp[2]
+        if (encontrado == false && (numsHelp[0] + 1) == numsHelp[1] && (numsHelp[1] + 1) == numsHelp[2]
             && (numsHelp[2] + 1) == numsHelp[3]) {
             ec++
-
+            encontrado = true
         }
 
         //escalera simple
-        if ((numsHelp[0] + 1) == numsHelp[1] && (numsHelp[1] + 1) == numsHelp[2]) {
+        if (encontrado == false && (numsHelp[0] + 1) == numsHelp[1] && (numsHelp[1] + 1) == numsHelp[2]) {
             es++
+            encontrado = true
 
-        } else if ((numsHelp[1] + 1) == numsHelp[2] && (numsHelp[2] + 1) == numsHelp[3]) {
+        } else if (encontrado == false && (numsHelp[1] + 1) == numsHelp[2] && (numsHelp[2] + 1) == numsHelp[3]) {
             es++;
+            encontrado = true
 
         }
 
         //trios
-        if ((numsHelp[0]) == numsHelp[1] && (numsHelp[1]) == numsHelp[2]) {
+        if (encontrado == false && (numsHelp[0]) == numsHelp[1] && (numsHelp[1]) == numsHelp[2]) {
             tr++
+            encontrado=true
 
-        } else if ((numsHelp[1]) == numsHelp[2] && (numsHelp[2]) == numsHelp[3]) {
+        } else if (encontrado == false && (numsHelp[1]) == numsHelp[2] && (numsHelp[2]) == numsHelp[3]) {
             tr++;
+            encontrado=true
         }
 
         //doble pareja
-        if (numsHelp[0] == numsHelp[1] && numsHelp[2] == numsHelp[3]) {
+        if (encontrado == false && numsHelp[0] == numsHelp[1] && numsHelp[2] == numsHelp[3]) {
             dp++;
+            encontrado=true
         }
 
 
-
+        encontrado=false
         nums = incrementar(nums)//incrementar *NO TOCAR*
     }
 
@@ -81,9 +90,90 @@ function calcularA() {
 
     calcularPorcetajes(PORpoker, poker, PORec, ec, PORes, es, PORtr, tr, PORdp, dp)
 
+}
 
 
+function calcularB(num) {
 
+    let figura = ""
+    let encontrado = false
+
+    //ordenado el array
+    numHelp = Array.from(num)
+    numHelp = numHelp.sort()
+
+    //comporbar si es un poker
+    if (encontrado == false && num[0] == num[1] && num[1] == num[2]
+        && num[2] == num[3] && num[3] == num[0]) {
+
+        figura = "poker"
+        encontrado = true
+    }
+
+    //escalera completa
+    if (encontrado == false && (numHelp[0] + 1) == numHelp[1] && (numHelp[1] + 1) == numHelp[2]
+        && (numHelp[2] + 1) == numHelp[3]) {
+        figura = "escalera completa"
+        encontrado = true
+
+    }
+
+    //escalera simple
+    if (encontrado == false && (numHelp[0] + 1) == numHelp[1] && (numHelp[1] + 1) == numHelp[2]) {
+        figura = "escalera simple"
+        encontrado = true
+
+    } else if (encontrado == false && (numHelp[1] + 1) == numHelp[2] && (numHelp[2] + 1) == numHelp[3]) {
+        figura = "escalera simple"
+        encontrado = true
+
+    }
+
+    //trios
+    if (encontrado == false && (numHelp[0]) == numHelp[1] && (numHelp[1]) == numHelp[2]) {
+        figura = "trio"
+        encontrado = true
+
+    } else if (encontrado == false && (numHelp[1]) == numHelp[2] && (numHelp[2]) == numHelp[3]) {
+        figura = "trio"
+        encontrado = true
+
+    }
+
+    //doble pareja
+    if (encontrado == false && numHelp[0] == numHelp[1] && numHelp[2] == numHelp[3]) {
+        figura = "doble pareja"
+        encontrado = true
+
+    }
+
+
+    document.getElementById("resultadoB").innerHTML = figura
+}
+
+function pedirNum() {
+    //numero de usuario
+    let nUsuario = document.getElementById("matricula").value
+
+    // comproar si es un numero de 4 digitos
+    if (nUsuario >= 0 && nUsuario != /[A-Z]/ && nUsuario.length == 4) {
+
+        //paso el *string* del numero a int y a un array
+        let num = Array.from(nUsuario)
+        num = pasarArrInt(num)
+
+        calcularB(num)
+
+    } else {
+        alert("Solo numero positivos de 4 digitos");
+    }
+}
+
+function pasarArrInt(num) {//combertir array de carcteres a numeros enteros
+    for (let index = 0; index < num.length; index++) {
+        num[index] = parseInt(num[index])
+    }
+    return num
 }
 
 function calcularPorcetajes/*y mostrar resultados*/(PORpoker, poker, PORec, ec, PORes, es, PORtr, tr, PORdp, dp) {
@@ -94,7 +184,13 @@ function calcularPorcetajes/*y mostrar resultados*/(PORpoker, poker, PORec, ec, 
     PORtr = tr * 100 / 10000
     PORdp = dp * 100 / 10000
 
-    
+    document.getElementById("PORpoker").innerHTML = PORpoker
+    document.getElementById("PORescaleraCompleta").innerHTML = PORec
+    document.getElementById("PORescaleraSimple").innerHTML = PORes
+    document.getElementById("PORtrio").innerHTML = PORtr
+    document.getElementById("PORdoblePareja").innerHTML = PORdp
+
+
 
 }
 
