87fe6142f41b2751376d2938a8331725b23b2b5a
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-12-15 11:13:23 +0100

    repaso

---
+++ JavaScript/T1/examen EV/repaso/main.js
@@ -0,0 +1,22 @@
+function abrirVentana1() {
+    let ventana1 = window.open("","ventana1","width=300 height=300 left=0, top=0")
+    ventana1.document.bgColor="green"
+    
+}
+
+
+function abrirVentana2() {
+    let ventana2 = window.open("","ventana2","width=300 height=300 left=2000, top=0")
+    ventana2.document.bgColor="blue"
+    
+}
+
+
+
+
+
+function abrirVentana3() {
+    let ventana3 = window.open("","ventana3","width=300 height=300 left=1000, top=2000")
+    ventana3.document.bgColor="pink"
+}
+
